da9b24bc6b121d62230c52badf9af02608f19219
Erik Heinz <jerik@gmx.net>
2024-12-12 00:45:13 +0100

    changed name of js file

--- smoncal.js
+++
@@ -1,8 +0,0 @@
-/** Add a new calculation line **/
-function add_calc_line() {
-	const cline = document.createElement('calc-line');
-	cline.setAttribute('contenteditable', 'true'); 
-	const csheet = document.getElementById('sheet');
-	console.log(csheet);
-	csheet.appendChild(cline); 
-}
