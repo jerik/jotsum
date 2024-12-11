5d24b8f0a2f9396f361c0e352b986e9aa6848f81
Erik Heinz <jerik@gmx.net>
2024-12-12 00:21:38 +0100

    removed old jest tests

--- tests/nebenrechnung.spec.js
+++ tests/nebenrechnung.spec.js
@@ -1,7 +1,9 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+// @todo setup web server with correct ssl, perhaps via docker?
 // disable HTTPS errors
+// https://stackoverflow.com/a/75547151/1933185
 test.use({
   ignoreHTTPSErrors: true,
 });
@@ -9,7 +11,7 @@ test.use({
 // test nebenrechnung
 test('initial start of page', async ({ page }) => {
   await page.goto('https://localhost:8008');
-	//
+
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/nebenrechnung/);
 });
