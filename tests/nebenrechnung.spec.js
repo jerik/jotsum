39df55b1b4997c24e05646666572dd589640baf0
Erik Heinz <jerik@gmx.net>
2024-12-13 23:53:03 +0100

    add button creates new line and sum

--- tests/nebenrechnung.spec.js
+++ tests/nebenrechnung.spec.js
@@ -15,41 +15,29 @@ test('initial start of page', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/nebenrechnung/);
+
+  const nrl = await page.locator('//nr-line').count(); 
+  const nrs = await page.locator('//nr-sum').count(); 
+  await expect(nrl).toBe(1); 
+  await expect(nrs).toBe(1); 
 });
 
 // https://playwright.dev/docs/running-tests
 test('button creates new calc line', async ({ page }) => {
   await page.goto('https://localhost:8008');
 
-  const before = await page.locator('//nr-line').count(); 
+  const nrl_before = await page.locator('//nr-line').count(); 
+  const nrs_before = await page.locator('//nr-sum').count(); 
   // console.log(before); 
 
   // Expect a title "to contain" a substring.
   await page.locator('button:text("Add line")').click();
   // https://www.marketingscoop.com/tech/web-scraping/playwright-how-to-find-elements-by-xpath-in-playwright/
-  const after = await page.locator('//nr-line').count(); 
+  const nrl_after = await page.locator('//nr-line').count(); 
+  const nrs_after = await page.locator('//nr-sum').count(); 
 
   // https://playwright.dev/docs/test-assertions
-  await expect(after).toBe(before + 1); 
-});
-
-// https://pkerschbaum.com/blog/using-playwright-to-run-unit-tests .  trifft es nicht ganz
-
-
-// simple unit test of function 
-// https://playwright.dev/docs/evaluating
-// FUNKTIONIERT auch OHNE WEBSERVER und MUSS NUR MIT EINEM BROWSER AUFGERUFEN WERDEN
-// npx playwright test foo.spec.js --project chromium
-test('unit test', async ({ page }) => {
-
-	// Get current working directory
-	// console.log(`Current directory: ${process.cwd()}`);
-
-	await page.addScriptTag({path: 'sum.js'});
-	const data = await page.evaluate(() => window.sum(1,7));
-	// await seems not to be needed, but I use it anyway
-	await expect(data).toBe(15); // 1+2*7=15
-	// console.log(data); 
-	
+  await expect(nrl_after).toBe(nrl_before + 1); 
+  await expect(nrs_after).toBe(nrs_before + 1); 
 });
 
