54fab89389021fad7b8b31326519af34094d27db
Erik Heinz <jerik@gmx.net>
2024-12-13 23:45:05 +0100

    add button creates line and sum box

--- nebenrechnung.js
+++ nebenrechnung.js
@@ -1,8 +1,10 @@
 /** Add a new calculation line **/
 function add_calc_line() {
-	const cline = document.createElement('nr-line');
-	cline.setAttribute('contenteditable', 'true'); 
-	const csheet = document.getElementById('sheet');
-	console.log(csheet);
-	csheet.appendChild(cline); 
+	const nr_line = document.createElement('nr-line');
+	nr_line.setAttribute('contenteditable', 'true'); 
+	const nr_sum = document.createElement('nr-sum');
+	const nr_sheet = document.getElementById('sheet');
+	console.log(nr_sheet);
+	nr_sheet.appendChild(nr_line); 
+	nr_sheet.appendChild(nr_sum); 
 }
