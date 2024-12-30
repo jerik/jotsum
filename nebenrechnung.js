70f6016807c2fa3ba6fb2b6145749bae7608e1a3
Erik Heinz <jerik@gmx.net>
2024-12-30 11:32:27 +0100

    calculation on every keystroke

--- nebenrechnung.js
+++ nebenrechnung.js
@@ -4,7 +4,7 @@ function add_calc_line() {
 	nr_line.setAttribute('contenteditable', 'true'); 
 	const nr_sum = document.createElement('nr-sum');
 	const nr_sheet = document.getElementById('sheet');
-	console.log(nr_sheet);
+	// console.log(nr_sheet);
 	nr_sheet.appendChild(nr_line); 
 	nr_sheet.appendChild(nr_sum); 
 }
@@ -15,17 +15,28 @@ class Nebenrechnung {
 
 	constructor() {
 		this.#get_sheet();
+		this.#key_listener(); 
+	}
+
+	#key_listener() {
+		// console.log('key_listener');
+		document.body.addEventListener('keyup', this.#keyups.bind(this));
+	}
+
+	#keyups(e) {
+		// console.log(e.code);
+		this.#get_sheet();
 	}
 
 	#get_sheet() {
 		const sheet = document.querySelector('nr-sheet');
 
 		Array.from(sheet.children).forEach((child, index) => {
-			console.log(`Element ${index + 1}:`, child.tagName, child.textContent.trim());
+			// console.log(`Element ${index + 1}:`, child.tagName, child.textContent.trim());
 			if (child.tagName == 'NR-LINE') {
 				this.#read_line(child); 
 			} else if (child.tagName == 'NR-SUM') {
-				console.log(child);
+				// console.log(child);
 				this.#to_sum(child, index);
 			}
 
@@ -45,26 +56,17 @@ class Nebenrechnung {
 			} else if( item.length == 1 && item.match( /([-()+*/%])/ )) { 
 				calc.push(item);
 			} else {
-				console.log(`ELSE: ${item}`);
+				// console.log(`ELSE: ${item}`);
 			}
 		})
-		console.log(calc);
+		// console.log(calc);
 		this.subtotal = eval(calc.join(''));
-		console.log(this.subtotal);
+		// console.log(this.subtotal);
 	}
 
 	#to_sum(tag, value) {
 		tag.textContent = this.subtotal; 
-		console.log(`to_sum: ${value + 1}`);
-	}
-
-	#rm_text(item) {
-		console.log('foo');
-		// console.log(item);
-	}
-
-	foo(txt) {
-		return txt;
+		// console.log(`to_sum: ${value + 1}`);
 	}
 
 }
